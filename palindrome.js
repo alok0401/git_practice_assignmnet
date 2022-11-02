dca6156ccf98d2fe44d3c4f8faf35ac5b70fb8f5
alok0401 <alokbanik35@gmail.com>
2022-11-02 17:33:43 +0530

    added

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let str="racecar";
+function reverse(str){
+let bag="";
+for(let i=str.length-1;i=>0;i--){
+
+}
+return bag;
+}
+let reversed=reverse(str);
+
+if(str==reversed){
+console.log("Palindrome");
+}else{
+console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,7 @@
+function checkPrime(num){
+for(let i=0;i<=num;i++){
+if(num%i){
+return true;}
+}
+return false;
+}
